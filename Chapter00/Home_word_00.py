58f47c4636521b32cc6ccc7c0b4b28d897c53500
vpslinuxinstall <shoparoundwyz@gmail.com>
2020-03-18 12:14:21 -0400

    commit

---
+++ Chapter00/Home_word_00.py
@@ -0,0 +1,20 @@
+# Peter Lulu
+# CS 100 2013F Section 01
+# HW 00, Sept 5, 2013
+
+# Exercise 5b
+Months = 12
+days = 31
+hour = 1
+
+# Exercise 5c
+height = 7.63
+length = 1.1
+width = 2.2
+
+# Exercise 5d
+manFromUncle = 'Napolean Solo'
+major = "Computer Science"
+city = "Boston"
+
+
