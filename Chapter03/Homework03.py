4ecc774fdef796e117bb57c175ed809a061b26f8
vpslinuxinstall <shoparoundwyz@gmail.com>
2020-03-18 19:26:07 -0400

    commit

---
+++ Chapter03/Homework03.py
@@ -0,0 +1,29 @@
+a = 3
+b = 4
+c = 5
+
+if a < b:
+    print("OK")
+if c < b:
+    print("OK")
+if a + b == c:
+    print("OK")
+if a * a + b * b == c * c:
+    print("OK")
+
+if a < b:
+    print("OK")
+else:
+    print("NOT OK")
+if c < b:
+    print("OK")
+else:
+    print("NOT OK")
+if a + b == c:
+    print("OK")
+else:
+    print("NOT OK")
+if a * a + b * b == c * c:
+    print("OK")
+else:
+    print("NOT OK")
---
+++ Chapter03/Lecture03.md
@@ -0,0 +1,11 @@
+# Lecture 03
+
+## Python program
+
+A Python program is a sequence of Python statement        
+
+## Built-in function print()
+
+- the quote marks around the string are omitted       
+- a print statement ends its output with a newline
+
