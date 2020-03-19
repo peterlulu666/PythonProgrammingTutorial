fbcb113f777dcce1aede5a6f8ce92d6353498c1e
vpslinuxinstall <shoparoundwyz@gmail.com>
2020-03-19 18:02:01 -0400

    commit

---
+++ Chapter04/Homework04.py
@@ -0,0 +1,27 @@
+months = ["January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December"]
+months_count = 0
+while months_count < 12:
+    if months[months_count][0] == "J":
+        print(months[months_count])
+    months_count = months_count + 1
+
+for number in range(0, 100):
+    if number % 2 == 0 and number % 5 == 0:
+        print(number)
+
+horton = "A person's a person, no matter how small."
+vowels = "aeiouAEIOU"
+for letter in horton:
+    if letter in vowels:
+        print(letter)
